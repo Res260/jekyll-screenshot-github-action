70b87d7a2de5d81ff8b4154d379b557f09ded2a6
Émilio Gonzalez <egg997@gmail.com>
2022-03-08 22:34:46 -0500

    full page screenshot + good resolution

--- screenshot.py
+++ screenshot.py
@@ -10,17 +10,11 @@ import signal
 import pyppeteer
 
 async def main():
-    print(1, flush=True)
-    browser = await pyppeteer.launch(args=['--no-sandbox'])
-    print(2, flush=True)
+    browser = await pyppeteer.launch(args=['--no-sandbox', '-size=1920,1080'])
     page = await browser.newPage()
-    print(3, flush=True)
     await page.goto('http://localhost:8000')
-    print(4, flush=True)
-    await page.screenshot({'path': '/github/home/screenshot.png'})
-    print(5, flush=True)
+    await page.screenshot({'path': '/github/home/screenshot.png', 'fullPage': True})
     await browser.close()
-    print(6, flush=True)
 
 with subprocess.Popen(['python', '-m', 'http.server'], cwd='/github/workspace/_site', stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as p:
     time.sleep(3.0)
